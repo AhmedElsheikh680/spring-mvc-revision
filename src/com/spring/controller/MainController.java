7841d6879860df9a4d72a51f0ad50563ddf1ee79
Ahmed Elsheikh <ahmedelsheikh680@gmail.com>
2021-06-07 22:39:08 +0200

    @Requestmapping

--- src/com/spring/controller/MainController.java
+++ src/com/spring/controller/MainController.java
@@ -4,10 +4,12 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequestMapping("/spring")
 public class MainController {
 	
 	@RequestMapping("/")
